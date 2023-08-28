288839a7c122c0285d68ce0fe657ad2f6224d9d6
fatiha <fathiato@gmail.com>
2023-08-28 08:46:54 +0100

    correcting linters

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,6 +1,10 @@
-class Api::GreetingsController < ApplicationController
-  def index
-    @greet = Greeting.order('RANDOM()').first
-    render json: @greet
+# frozen_string_literal: true
+
+module Api
+  class GreetingsController < ApplicationController
+    def index
+      @greet = Greeting.order('RANDOM()').first
+      render json: @greet
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,8 +7,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-first_greeting = Greeting.create(greeting: "Hello There!")
-second_greeting = Greeting.create(greeting: "How do you do!")
-third_greeting = Greeting.create(greeting: "Bonjour!")
-fourth_greeting = Greeting.create(greeting: "Salam Alaykum!")
-fifth_greeting = Greeting.create(greeting: "Good morning!")
+Greeting.create(greeting: 'Hello There!')
+Greeting.create(greeting: 'How do you do!')
+Greeting.create(greeting: 'Bonjour!')
+Greeting.create(greeting: 'Salam Alaykum!')
+Greeting.create(greeting: 'Good morning!')
